3122fd1c2aa5f764ecc70da24648f385a2a46605
xiaonimo <xiaonimo666@gmail.com>
2016-09-18 16:47:27 +0800

    print 1 to 100 without loop or recursion

---
+++ 000_print1to100.cpp
@@ -0,0 +1,21 @@
+﻿/* 题目：
+ * 不使用循环或者递归，如何从1打印到100
+ */
+#include<iostream>  
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	static int a;
+	Solution(){ cout << a++ << endl; }
+};
+int Solution::a = 1;
+int main(){
+	Solution s[100];
+	system("pause");
+	return 0;
+}
