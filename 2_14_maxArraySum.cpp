6aa2e79864edfaad0ff475987d0a25ca47a06a10
xiaonimo <xiaonimo666@gmail.com>
2016-10-01 15:03:51 +0800

    Max array sum

---
+++ 2_14_maxArraySum.cpp
@@ -0,0 +1,35 @@
+﻿/* Title:
+ * The max sum of the array
+ */
+#include<iostream>  
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<bitset>
+#include<cassert>
+
+using namespace std;
+
+class Solution {
+public:
+	//动态规划
+	//sum是当前子数组中最大子数组和
+	int getMaxSum(vector<int> nums) {
+		int sz = nums.size();
+		int sum = nums[sz - 1];
+		int beg = nums[sz - 1];
+		for (int i = sz - 2; i >= 0; --i) {
+			beg = max(nums[i], nums[i] + beg);
+			sum = max(sum, beg);
+		}
+		return sum;
+	}
+};
+
+int main(void)
+{
+	Solution s;
+	cout << s.getMaxSum(vector<int>{-9, -2, -3, -5, -3});
+	system("pause");
+	return 0;
+}
